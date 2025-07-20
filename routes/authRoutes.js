deed58c7d944da3bd20ea52017ca681bab614008
Felipe Costa <fel.oliveira.costa@outlook.com>
2025-07-20 20:57:43 -0300

    refazer

--- routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { registerUser } = require('/home/felipe-costa/Documents/Estudos/site/controllers/userController.js');
-const router = express.Router();
-
-router.post('/register', registerUser);
-
-module.exports = router;
